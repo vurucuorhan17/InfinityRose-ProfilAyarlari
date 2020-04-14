73231ad865a10c80c9199af3744016728d2c46c9
orhan <vurucuorhan17@gmail.com>
2020-04-14 16:34:47 +0300

    Password stored as hash

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const bcrypt = require("bcrypt");
 
 const Profil = require("../models/Profil");
 
@@ -14,16 +15,30 @@ router.post("/login",(req,res) => {
     Profil.findOne({mail},(err,user) => {
         if(user)
         {
-            if(user.password === password)
-            {
-                req.session.userId = user._id;
-                res.redirect("/");
+
+            bcrypt.compare(password,user.password,(err,result)=> {
+                if(result)
+                {
+                    req.session.userId = user._id;
+                    res.redirect("/");
+                }
+                else
+                {
+                    res.redirect("/users/login")
+                }
+
+            });
+
+            // if(user.password === password)
+            // {
+            //     req.session.userId = user._id;
+            //     res.redirect("/");
             
-            }
-            else
-            {
-                res.redirect("/users/login");
-            }
+            // }
+            // else
+            // {
+            //     res.redirect("/users/login");
+            // }
         }
         else
         {
@@ -50,10 +65,17 @@ router.post("/register",(req,res) => {
     let profileImage = req.files.picture;
     profileImage.mv(path.resolve(__dirname,"../public/img/profilImages",profileImage.name));
 
-    Profil.create({
-        ...req.body,
-        picture: `/img/profilImages/${profileImage.name}`
-    });
+    bcrypt.hash(req.body.password,10,(err,hash) => {
+        Profil.create({
+            "name":req.body.name,
+            "tel":req.body.tel,
+            "mail":req.body.mail,
+            "password":hash,
+            "address":req.body.address,
+            picture: `/img/profilImages/${profileImage.name}`,
+
+        });
+    })
     res.redirect("/"); 
 
 });
